904994866e5459052154f60e97eda799a1bb0334
Gislaine Amancio <gimcjf@gmail.com>
2021-09-28 10:15:17 -0300

    criação do metodo cabeçalho

---
+++ src/calculoMedia.java
@@ -0,0 +1,14 @@
+public class calculoMedia{
+
+    public static void main(String[] args) {
+        cabecalho();
+        
+    }
+
+    static void cabecalho(){
+        System.out.println("\n\t**************Calculo de media****************\n");
+    }
+
+    
+
+}
