c18988197b08a7f68d2fbe53f4ba72607526aff0
Gislaine Amancio <gimcjf@gmail.com>
2021-09-28 10:21:59 -0300

    criação do metodo media

--- src/calculoMedia.java
+++ src/calculoMedia.java
@@ -13,7 +13,11 @@ public class calculoMedia{
     static int somar(int n1, int n2){
         int resultado = n1 + n2;
         return resultado;
+    }
 
+    static double media(int n1, int n2, int n3, int n4){
+        double media = (n1 + n2 + n3 + n4) / 4;
+        return media;
     }
 
 
