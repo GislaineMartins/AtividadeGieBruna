15aa9beee026d07596c3ca3a997aeb8f00ecd2ca
Gislaine Amancio <gimcjf@gmail.com>
2021-09-28 10:19:02 -0300

    criação do metodo somar

--- src/calculoMedia.java
+++ src/calculoMedia.java
@@ -3,12 +3,19 @@ public class calculoMedia{
     public static void main(String[] args) {
         cabecalho();
         
+        
     }
 
     static void cabecalho(){
         System.out.println("\n\t**************Calculo de media****************\n");
     }
 
-    
+    static int somar(int n1, int n2){
+        int resultado = n1 + n2;
+        return resultado;
+
+    }
+
+
 
 }
