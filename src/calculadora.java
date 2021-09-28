941132fe12aa4e22ec30c11daf9acdcf3bf2832c
Gislaine Amancio <gimcjf@gmail.com>
2021-09-28 16:42:32 -0300

    criação do metodo cabecalho e menu

---
+++ src/calculadora.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class calculadora {
+    public static void main(String[] args) {
+        cabecalho();
+        menu();
+        
+    }
+
+    static void cabecalho(){
+        System.out.println("\n\t************Calculadora***********");
+    }
+
+    static void menu(){
+        System.out.println("\t1- Somar");
+        System.out.println("\t2- Subtrair");
+        System.out.println("\t3- Multiplicar");
+        System.out.println("\t4- Dividir");
+    }
+
+    
+
+
+    
+}
